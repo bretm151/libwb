2f98389e86c2884abba29171155ce540bb2c72e7
Bret McKee <bretm@boneheads.us>
2014-02-18 20:30:22 -0700

    wip

--- skel/mp11.cu
+++ skel/mp11.cu
@@ -101,7 +101,8 @@ __global__
 void gpu_castToUChar(const float *floatImage, PUCHAR ucharImage, int height, int width, int channels)
 {
 	const int tx = threadIdx.x;
-    const int idx = blockIdx.x*blockDim.x + tx;
+	const int ty = threadIdx.y;
+    const int idx = (blockIdx.y + ty)*width + blockIdx.x*blockDim.x + tx;
 
     if (idx < height*width)
     {
@@ -116,8 +117,8 @@ void gpu_castToUChar(const float *floatImage, PUCHAR ucharImage, int height, int
 cudaError_t
 castToUChar(const float *floatImage, PUCHAR ucharImage, int height, int width, int channels)
 {
-    dim3 dimBlock(1024, 1, 1);
-    dim3 dimGrid(1+(height*width-1)/dimBlock.x, 1, 1);
+    dim3 dimBlock(2, 2, 1);
+    dim3 dimGrid(1+(height-1)/dimBlock.x, 1+(width-1)/dimBlock.y, 1);
 
     float *gpu_floatImage=NULL;
     PUCHAR gpu_ucharImage=NULL;
@@ -135,8 +136,8 @@ castToUChar(const float *floatImage, PUCHAR ucharImage, int height, int width, i
 __global__
 void gpu_convertRGBToGray(const PUCHAR rgbImage, PUCHAR grayImage, int height, int width, int channels)
 {
-	const int ty = threadIdx.y;
 	const int tx = threadIdx.x;
+	const int ty = threadIdx.y;
     const int idx = (blockIdx.y + ty)*width + blockIdx.x*blockDim.x + tx;
 
     if (idx < height*width)
